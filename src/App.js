c8e28be43a13c9e137a148de9b7bc5ef815c5553
clotteydollar <gideonclottey@gmail.com>
2019-04-09 10:09:08 +0800

    ne changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
-import React, { Component } from 'react';
-import Todos from './components/todos'
-import AddTodo from './components/AddForm'
+import React, { Component } from "react";
+import Todos from "./components/todos";
+import AddTodo from "./components/AddForm";
 
 class App extends Component {
-  state ={
-    todos:[
-        {id:1, content:"buy some milk"},
-        {id:2, content:"Go to shoprite"}
-    ],
-    checked:false
-  }
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "Go to shoprite" }
+    ]
+  };
 
-  deleteTodo =(id) => { 
-    const todos = this.state.todos.filter(todo =>{
-      return todo.id !== id
-    })
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
 
     this.setState({
-      todos:todos
-    })
-  }
-  addTodo =(todo)=>{
-    todo.id =Math.random()
-    let todos =[...this.state.todos, todo]
+      todos: todos
+    });
+  };
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
     this.setState({
-      todos     
-    })
-  }
-  handleChecked =(e) =>{
-    this.setState({
-      checked:!this.state.checked
-    })
+      todos,
+      complete: false
+    });
+  };
 
-  }
+  isComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            id: todo.id,
+            content: todo.content,
+            complete: !todo.complete
+          };
+        } else {
+          return todo;
+        }
+      })
+    });
+  };
   render() {
     return (
       <div className="App container">
         <h1 className="center blue-text">Todo's</h1>
-        <AddTodo addTodo={this.addTodo}/>
-         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} handleChecked={this.handleChecked}/>
-         
+        <AddTodo addTodo={this.addTodo} />
+        <Todos
+          todos={this.state.todos}
+          deleteTodo={this.deleteTodo}
+          handleChecked={this.handleChecked}
+          isComplete={this.isComplete}
+        />
       </div>
     );
   }
--- src/components/todos.js
+++ src/components/todos.js
@@ -1,30 +1,25 @@
-import React from 'react'
+import React from "react";
 
-
-const Todos =({todos,deleteTodo, handleChecked})=>{
-
-    const todoList = todos.length ?(
-        todos.map(todo => {
-            return( 
-                <div className="collection-item" key={todo.id}>
-                    <input type="checkbox" onClick={handleChecked}/>
-                    <span  onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
-                    
-                    
-                </div>
-            )
-
-        })
-
-    ):(
-    <p className="center">You have no todos left</p>
-    )
-    return(
-        <div className="todos collection">
-            {todoList }
-
+const Todos = ({ todos, deleteTodo, isComplete }) => {
+  const completeHandler = () => {
+    isComplete(todos.id);
+  };
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div
+          style={{ textDecoration: todos.complete ? "line-through" : "" }}
+          onClick={completeHandler}
+          className="collection-item"
+          key={todo.id}
+        >
+          <span>{todo.content}</span>
         </div>
-
-    )
-}
+      );
+    })
+  ) : (
+    <p className="center">You have no todos left</p>
+  );
+  return <div className="todos collection">{todoList}</div>;
+};
 export default Todos;
